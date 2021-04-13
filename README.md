9a10c51f976d0e28ef19ebe70ea4778f76848259
Alexander Koc <alkc@users.noreply.github.com>
2021-04-13 10:45:05 +0200

    Upgrade to version 0.6

--- README.md
+++ README.md
@@ -7,3 +7,24 @@ You will need both `parallel` and structure installed — an easy task with cond
 ```
 conda install -c bioconda parallel structure
 ```
+
+## Example
+
+To check if the script works, please use the included example data set (borrowed from the structure website)
+
+Please run the following command from the script directory:
+
+```
+bash parallel-structure.sh example-data/mainparams example-data/extraparams example-data/testdata1 output_dir 1 3 5 8
+```
+
+The command starts 8 parallel jobs for K=1 to K=3 with 5 replicates for each tested value K.
+
+
+## CHANGELOG
+
+## New in version 0.6 <2021-04-13>
+
+* FIXED bug where replicate runs started with the same seed, which defeated the purpose of reps.
+* ADDED Ability to set min K, max K, number of reps and number of parallel jobs from the command line
+* ADDED More informative error messages if files missing at specified paths  
---
+++ parallel-structure.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+# "parallel-structure.sh"
+# Author: Alexander Koc <alexander.koc@slu.se>
+# Version: 0.6 <2021-04-13>
+# Description: Wrapper around structure allowing for parallel runs of multiple values of MAXPOP/K and replicates
+
+# Usage:
+# $ bash parallel-structure.sh path/to/mainparams path/to/extraparams path/to/input_data path/to/output_directory min_K max_K nbr_reps nbr_parallel_jobs
+
+if [ "$#" -eq  "0" ]
+then
+    echo ""
+    echo "parallel-structure.sh"
+    echo ""
+    echo "Author: Alexander Koc <alexander.koc@slu.se>"
+    echo "Version: 0.6 <2021-04-13>"
+    echo "Description: Wrapper around structure allowing for parallel runs of multiple values of MAXPOP/K and replicates"
+    echo ""
+    echo -e "\tUsage:"
+    echo -e "\t$ bash run-structure-analysis.sh path/to/mainparams path/to/extraparams path/to/input_data path/to/output_directory min_K max_K nbr_reps nbr_parallel_jobs"
+    echo ""
+    echo "For more instructions/examples, please refer to: https://github.com/alkc/parallel-structure"
+    echo ""
+    exit 0
+fi
+
+
+mainparams="$1"
+extraparams="$2"
+inputdata="$3"
+outputfolder="$4"
+k_min=$5
+k_max=$6
+n_reps=$7
+# Script parameters:
+n_parallel_jobs="$8"
+
+
+k_series=$(seq $k_min 1 $k_max)
+rep_series=$(seq 1 1 $n_reps)
+
+# Process settings:
+function file_exists {
+
+    file="$1"
+    
+    if [ ! -f "$file" ]
+    then
+	echo "[ERROR] $file does not exist."
+	exit 1
+    fi
+    
+}
+
+function run_structure {
+
+    rand_seed="$RANDOM"
+    
+    mainparams="$1"
+    extraparams="$2"
+    inputdata="$3"
+    outputfolder="$4"
+    curr_k="$5"
+    curr_rep="$6"
+
+    extraparams_tmp="/tmp/extraparams_K${curr_k}_REP${curr_rep}_${rand_seed}"
+
+    cat ${extraparams} > ${extraparams_tmp}
+    echo -e "" >> ${extraparams_tmp}
+    echo -e "#define SEED $rand_seed" >> ${extraparams_tmp}
+
+    sed '/RANDOMIZE/d' -i ${extraparams_tmp}
+    echo -e "#define RANDOMIZE 0" >> ${extraparams_tmp}
+    
+    # cat $extraparams_tmp
+
+    structure -m ${mainparams} -e ${extraparams_tmp} -i ${inputdata} -o ${outputfolder}/K${curr_k}_REP${curr_rep} -K ${curr_k} > ${outputfolder}/K${curr_k}_REP${curr_rep}.output.txt
+
+    rm ${extraparams_tmp}
+    
+}
+
+export -f run_structure
+
+# Script start
+echo "[START] Hello there."
+echo "[INFO] Script running with following parameters:"
+echo "[INFO] Input data: ${inputdata}"
+
+file_exists $inputdata
+
+echo "[INFO] Main parameter file: ${mainparams}"
+
+file_exists $mainparams
+
+echo "[INFO] Extra params file: ${extraparams}"
+
+file_exists $extraparams
+
+echo "[INFO] BTW. This file will be modified to add a random seed for each run. Custom seeds or RANDOMIZE=1 will be overriden."
+echo "[INFO] Output set to be saved in: ${outputfolder}"
+
+[ ! -d "$outputfolder" ] && echo "[INFO] Creating directory: $outputfolder" && mkdir -p $outputfolder
+
+echo "[INFO] Script will run ${n_parallel_jobs} jobs."
+echo "[INFO] Using Testing MAXPOP/K from K = ${k_min}-${k_max}"
+echo "[INFO] $n_reps reps per K"
+echo "[INFO] Buckle up. Starting the structure run in parallel." 
+
+# Execute structure in parallel over $n_threads. 
+parallel --progress -j ${n_parallel_jobs} run_structure ${mainparams} ${extraparams} ${inputdata} ${outputfolder} {1} {2} ::: "${k_series[@]}" ::: "${rep_series[@]}"
+
+echo "[INFO] All structure runs completed. (... Alternatively, all structure runs exited with errors)."
+echo "[EXIT] Bye."
